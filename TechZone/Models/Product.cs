17576b9f3efa465c1092d83030d63f173d86e47a
eslams3dawi <eslamseadawy717@gmail.com>
2025-03-13 15:40:29 +0200

    Modify Test

--- TechZone/Models/Product.cs
+++ TechZone/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int ProductId { get; set; }
-        public string Name { get; set; }
+        public string Name   { get; set; }
         public string? Description { get; set; }
         public double Price { get; set; }
         public int StockQuantity { get; set; }
